3ed8c5f34ec2270dab94c4523f554bcff53e04d4
Jehad Halahla <jehadkhaledh2002@gmail.com>
2023-05-08 18:49:41 +0300

    starting a new project

---
+++ three_sum.asm
@@ -0,0 +1,32 @@
+.data
+prompt: .asciiz "please enter 3 numbers\n"
+
+.text 
+ .globl main
+ main:
+ 	li $v0,4
+ 	la $a0,prompt
+ 	syscall 
+ 	
+	li $v0,5
+	syscall 
+	move $t0,$v0
+	
+	li $v0,5
+	syscall 
+	move $t1,$v0
+	
+	li $v0,5
+	syscall 
+	move $t2,$v0
+	
+	addu $t0,$t0,$t1
+	addu $t0,$t0,$t2
+	
+	
+	li $v0,1
+	move $a0,$t0
+	syscall 
+	
+	li $v0,10
+	syscall 
